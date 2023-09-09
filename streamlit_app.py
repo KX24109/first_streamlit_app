38da699515d24532ad984e25388ee42972f9bce6
KX24109 <144438455+KX24109@users.noreply.github.com>
2023-09-09 15:54:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 
-streamlit.header('Breakfast Menu')
+streamlit.title('My Mom New Healthy Dinner')
+
+streamlit.header('Breakfast Favorites')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
